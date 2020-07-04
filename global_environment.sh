a3ca992ad991413f2616a65fc6f863fef4a50484
Paweł Jarosz <jarosz.pawel@outlook.com>
2020-07-04 17:35:44 +0200

    Remove finding all ssh keys

--- global_environment.sh
+++ global_environment.sh
@@ -4,8 +4,10 @@ alias gdb='/usr/bin/gdb --tui'
 alias gitlog='git log --oneline --graph --decorate'
 
 # Add existing ssh keys
-eval "$(ssh-agent -s)" > /dev/null
-find ~/.ssh/ -type f -not -name "config" -and -not -name "known_hosts" -and -not -name "*.pub" -exec ssh-add {}  \; >/dev/null
+#eval "$(ssh-agent -s)" > /dev/null
+#find ~/.ssh/ -type f -not -name "config" -and -not -name "known_hosts" -and -not -name "*.pub" -exec ssh-add {}  \; >/dev/null
+
+# All ssh keys should be configured in ssh config
 
 source ~/.config/typos.sh
 

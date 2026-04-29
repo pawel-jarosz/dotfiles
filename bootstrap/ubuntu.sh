8b7c77d42008340267c73b85642104239767aa2b
Paweł Jarosz <jarosz.pawel@outlook.com>
2026-04-29 14:41:16 +0200

    Add bootstrap for ubuntu and fix install script

--- bootstrap/ubuntu.sh
+++ bootstrap/ubuntu.sh
@@ -3,3 +3,17 @@ sudo apt install -y pkg-config libssl-dev build-essential
 curl -s https://ohmyposh.dev/install.sh | bash -s
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo snap install --classic nushell
+
+curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+cd rclone-*-linux-amd64
+sudo cp rclone /usr/bin/
+sudo chown root:root /usr/bin/rclone
+sudo chmod 755 /usr/bin/rclone
+sudo mkdir -p /usr/local/share/man/man1
+sudo cp rclone.1 /usr/local/share/man/man1/
+sudo mandb
+rclone config
+cd ..
+rm -rf rclone*
+

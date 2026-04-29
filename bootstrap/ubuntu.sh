51503caeb575f616c99f417233cd281f3686cb09
Paweł Jarosz <jarosz.pawel@outlook.com>
2026-04-29 14:25:12 +0200

    Add bootstrap for ubuntu and missing package

---
+++ bootstrap/ubuntu.sh
@@ -0,0 +1,5 @@
+sudo apt install -y fzf cmake neovim unzip
+sudo apt install -y pkg-config libssl-dev build-essential
+curl -s https://ohmyposh.dev/install.sh | bash -s
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo snap install --classic nushell

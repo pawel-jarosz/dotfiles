853ac9f3b1e81af83f09fb80b7233ef3e21631cf
Paweł Jarosz <jarosz.pawel@outlook.com>
2026-04-29 12:03:27 +0200

    Add missing packages

--- bootstrap/windows.ps1
+++ bootstrap/windows.ps1
@@ -8,6 +8,11 @@ winget install neovim.neovim
 winget install junegunn.fzf
 winget install JanDeDobbeleer.OhMyPosh -s winget
 winget install sharkdp.bat
+winget install sharkdp.fd
 winget install RARLab.WinRAR
 winget install Ghisler.TotalCommander
+winget install Rclone.Rclone
+
 oh-my-posh font install
+mkdir ([$env.APPDATA, "nushell", "vendor", "autoload"] | path join)
+
